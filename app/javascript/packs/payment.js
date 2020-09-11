f7047e433d1a333404e95481512eb3aec486608c
Juan M. Medina <juanmanuelmedina@hotmail.com>
2020-09-10 23:04:34 -0500

    Initial commit

--- app/javascript/packs/payment.js
+++ app/javascript/packs/payment.js
@@ -9,64 +9,63 @@ async function getToken() {
 }
 
 getToken().then(async (client_token) => {
-
-  const button = document.querySelector('#google-pay-button');
   const paymentsClient = new google.payments.api.PaymentsClient({
-    environment: 'TEST' // Or 'PRODUCTION'
+    environment: 'TEST' // FOR PRODUCIION SET TO  'PRODUCTION'
   });
 
-  await braintree.client.create({
+  const clientInstance = await braintree.client.create({
     authorization: client_token
-  }).then(function (clientInstance) {
-    return braintree.googlePayment.create({
-      client: clientInstance,
-      googlePayVersion: 2,
-      googleMerchantId: null // Optional in sandbox; if set in sandbox, this value must be a valid production Google Merchant ID
-      // googleMerchantId: 'merchant-id-from-google' // Optional in sandbox; if set in sandbox, this value must be a valid production Google Merchant ID
-    });
-    // }).then(function (googlePaymentErr, googlePaymentInstance) {
-  }).then(function (googlePaymentInstance) {
-    return paymentsClient.isReadyToPay({
-      // see https://developers.google.com/pay/api/web/reference/object#IsReadyToPayRequest
-      apiVersion: 2,
-      apiVersionMinor: 0,
-      allowedPaymentMethods: googlePaymentInstance.createPaymentDataRequest().allowedPaymentMethods,
-      existingPaymentMethodRequired: true // Optional
-    });
-  }).then(function (response) {
-    if (response.result) {
-      button.addEventListener('click', function (event) {
-        event.preventDefault();
-
-        var paymentDataRequest = googlePaymentInstance.createPaymentDataRequest({
-          transactionInfo: {
-            currencyCode: 'USD',
-            totalPriceStatus: 'FINAL',
-            totalPrice: '100.00' // Your amount
-          }
-        });
-
-        // We recommend collecting billing address information, at minimum
-        // billing postal code, and passing that billing postal code with all
-        // Google Pay card transactions as a best practice.
-        // See all available options at https://developers.google.com/pay/api/web/reference/object
-        var cardPaymentMethod = paymentDataRequest.allowedPaymentMethods[0];
-        cardPaymentMethod.parameters.billingAddressRequired = true;
-        cardPaymentMethod.parameters.billingAddressParameters = {
-          format: 'FULL',
-          phoneNumberRequired: true
-        };
-
-        paymentsClient.loadPaymentData(paymentDataRequest).then(function (paymentData) {
-          return googlePaymentInstance.parseResponse(paymentData);
-        }).then(function (result) {
-          alert('Success, the nonce is ' + result);
-        }).catch(function (err) {
-          alert('Failure ' + error);
-        });
-      });
-    }
-  }).catch(function (err) {
-    alert('General Error' + err);
   });
+  
+  const googlePaymentInstance = await braintree.googlePayment.create({
+    client: clientInstance,
+    googlePayVersion: 2,
+    googleMerchantId: null // FOR PRODUCITON NEEDS THE MERCHANT ID POPULATED HERE!
+  });
+  
+  const isReadyToPay = await paymentsClient.isReadyToPay({
+    apiVersion: 2,
+    apiVersionMinor: 0,
+    allowedPaymentMethods: googlePaymentInstance.createPaymentDataRequest().allowedPaymentMethods,
+    existingPaymentMethodRequired: true // MAY NEED TO PLAY WITH THIS FOR A BRAND NEW GOOGLE PAY USER
+  });
+
+  if (!isReadyToPay.result) {
+    throw 'Google Instance is not ready to Pay';
+  }
+
+  const buttonClickHandler = function (event) {
+    event.preventDefault();
+
+    var paymentDataRequest = googlePaymentInstance.createPaymentDataRequest({
+      transactionInfo: {
+        currencyCode: 'USD',
+        totalPriceStatus: 'FINAL',
+        totalPrice: '1.00' // Your amount
+      }
+    });
+
+    var cardPaymentMethod = paymentDataRequest.allowedPaymentMethods[0];
+    cardPaymentMethod.parameters.billingAddressRequired = true;
+    cardPaymentMethod.parameters.billingAddressParameters = {
+      format: 'MIN'
+    };
+
+    paymentsClient.loadPaymentData(paymentDataRequest).then(function (paymentData) {
+      return googlePaymentInstance.parseResponse(paymentData);
+    }).then(function (result) {
+      alert('Success, the nonce is ' + result.nonce);
+    }).catch(function (err) {
+      alert('Failure ' + err);
+    });
+  };
+
+  const buttonContainer = document.querySelector('#button-container');
+  const button = paymentsClient.createButton({
+    buttonType: 'long',
+    onClick: buttonClickHandler
+  });
+  buttonContainer.appendChild(button);
+}).catch((error) => {
+  alert('Error processing payment - ' + error);
 });
